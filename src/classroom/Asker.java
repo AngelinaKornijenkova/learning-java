d631d80e351e6bac9ed117786e573fb413278dd6
AngelinaKornijenkova <angelinamartirosjana@gmail.com>
2024-03-17 18:11:20 +0200

    [homework/16032024]: Homework 06.03.2024

---
+++ src/classroom/Asker.java
@@ -0,0 +1,17 @@
+package classroom;
+
+import java.util.Scanner;
+
+public class Asker {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String answer = reader.nextLine();
+        if(answer.equals("John")) {
+            System.out.println("John is not allowed here!");
+        } else {
+            System.out.println("Welkome: " + answer);
+        }
+
+    }
+}
